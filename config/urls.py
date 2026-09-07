23cd65cb51d45d9386bccf05cbb281ecbbf96516
musaib-ch <164421240+musaib-ch@users.noreply.github.com>
2026-09-07 16:24:19 +0500

    Replace project code with uploaded fixed version

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from tickets.api import TicketViewSet
 from users.api import UserViewSet, TeamViewSet
 
@@ -14,8 +13,6 @@ router.register(r'departments', TeamViewSet, basename='team')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include(router.urls)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('manage/', include('users.urls')),
