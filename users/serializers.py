1ec389a14fa26df2118c6bc6696aad20045762d6
musaib-ch <164421240+musaib-ch@users.noreply.github.com>
2026-09-07 16:27:16 +0500

    Replace user serializers with uploaded fixed version

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from .models import User, Team
 
+
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = ['id', 'name', 'description', 'manager', 'created_at']
 
+
 class UserSerializer(serializers.ModelSerializer):
-    team_name = serializers.CharField(source='team.name', read_only=True)
-    
+    teams = TeamSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'team', 'team_name', 'status', 'ad_username']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'teams', 'status']
