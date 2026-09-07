4f2bc32a7120dad12d40ec40342e4ef379209833
musaib-ch <164421240+musaib-ch@users.noreply.github.com>
2026-09-07 16:27:30 +0500

    Replace user URLs with uploaded fixed version

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('departments/<int:team_id>/edit/', views.edit_team, name='edit_team'),
     path('departments/<int:team_id>/delete/', views.delete_team, name='delete_team'),
     path('branding/', views.manage_branding, name='manage_branding'),
-    path('smtp/', views.manage_smtp, name='manage_smtp'),
     path('profile/', views.profile_setup, name='profile_setup'),
     path('auth/microsoft/login/', views.microsoft_login, name='microsoft_login'),
     path('auth/microsoft/callback/', views.microsoft_callback, name='microsoft_callback'),
